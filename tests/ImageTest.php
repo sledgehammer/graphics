ee5974c6db4290a88a9e2f19ee60bf34122d8907
Bob Fanger <bfanger@gmail.com>
2016-02-26 11:26:42 +0100

    Sledgehammer 16 upgrade

--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -1,29 +1,33 @@
 <?php
-namespace Sledgehammer;
+
+namespace SledgehammerTests\Graphics;
+
+use Sledgehammer\Graphics\Image;
+use SledgehammerTests\Core\TestCase;
+
 /**
- * Image test
- *
- * @package GD
+ * Image test.
  */
-class ImageTest extends TestCase {
+class ImageTest extends TestCase
+{
+    public function test_jpg()
+    {
+        $Image = new Image(dirname(__FILE__).'/images/test.jpg');
+        $this->assertEquals($Image->width, 132);
+    }
 
-	function test_jpg() {
-		$Image = new Image(dirname(__FILE__).'/images/test.jpg');
-		$this->assertEquals($Image->width, 132);
-	}
+    public function test_invalid_extension_warning()
+    {
+        $this->setExpectedException('PHPUnit_Framework_Error_Warning');
+        $filename = dirname(__FILE__).'/images/jpeg_file.gif';
+        $image = new Image($filename);
+        $image->height;
+    }
 
-	function test_invalid_extension_warning() {
-		$this->setExpectedException('PHPUnit_Framework_Error_Warning');
-		$filename = dirname(__FILE__).'/images/jpeg_file.gif';
-		$image = new Image($filename);
-		$image->height;
-	}
-
-	function test_extension_autocorrection() {
-		$filename = dirname(__FILE__).'/images/jpeg_file.gif';
-		$image = new Image($filename);
-		$this->assertEquals(@$image->width, 132);
-	}
-
+    public function test_extension_autocorrection()
+    {
+        $filename = dirname(__FILE__).'/images/jpeg_file.gif';
+        $image = new Image($filename);
+        $this->assertEquals(@$image->width, 132);
+    }
 }
-?>
